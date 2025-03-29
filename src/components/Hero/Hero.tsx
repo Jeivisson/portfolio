d9954511618e4170cc3d007a162256988a2a8491
Jeivisson <jeivisson@ymail.com>
2025-03-29 18:57:10 -0300

    react-icons add

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
 import SVGIMG from '../../../public/images/profile.svg';
+import { RiNextjsLine } from 'react-icons/ri';
 
 export default function Hero() {
   return (
-    <div>
+    <div className="border-blue-100 border">
+      <RiNextjsLine />
+
       <Image src={SVGIMG} width={200} height={200} alt="my profile" />
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Layout from '@/components/Layout/Layout';
 
+import '@/styles/global.css';
+
 export default function Home() {
   return <Layout />;
 }
